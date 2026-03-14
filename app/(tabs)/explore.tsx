2e0d69f7b1c1974aedc0e90b55415ba9ef493e68
Elliot Datsomor <datsomorelliot56@gmail.com>
2026-03-14 22:35:05 +0000

    message from the lord hahaha

--- app/(tabs)/explore.tsx
+++ app/(tabs)/explore.tsx
@@ -56,7 +56,7 @@ export default function TabTwoScreen() {
         <ThemedText>
           For static images, you can use the <ThemedText type="defaultSemiBold">@2x</ThemedText> and{' '}
           <ThemedText type="defaultSemiBold">@3x</ThemedText> suffixes to provide files for
-          different screen densities
+          different screen densities nothing that the Expo Image component will automatically pick the best one for the device.
         </ThemedText>
         <Image
           source={require('@/assets/images/react-logo.png')}
